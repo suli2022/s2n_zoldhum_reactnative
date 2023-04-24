39b7329ae6c6e1d3575b00d502e076342557bab5
Sallai András <janos@zold.lan>
2023-04-24 08:50:46 +0200

    A listába renderelés kész

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
+  const [emps, setEmps] = useState([]);
   function getEmployees() {
     let host = 'http://localhost:8000/api/';
     let endpoint = 'employees';
     let url = host + endpoint;
 
     fetch(url)
-    .then(result => result.json())
-    .then(response => {
-      console.log(response)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result)
+      setEmps(result)
     });
 
   }
   useEffect( () => {
     getEmployees();
-  });
+  }, []);
   
   return (
     <View style={styles.container}>
       <Text>Zolhum</Text>
+
+      <FlatList 
+      data={emps}
+      renderItem={ ({item}) => (
+        <Text>{item.name}</Text>
+      )}
+      />
 
 
 
